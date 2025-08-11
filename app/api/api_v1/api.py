2c35b93decf8f2da641b851ea681c4878b3d9e92
mohamed younes <senoussi.med.younes@gmail.com>
2025-08-11 03:10:01 +0100

    remove some env vars

--- app/api/api_v1/api.py
+++ app/api/api_v1/api.py
@@ -11,8 +11,8 @@ api_router = APIRouter(
 )
 
 api_router.include_router(auth.router, prefix="/auth", tags=["authentication"])
-api_router.include_router(photos.router, prefix="/photos", tags=["photo"])
+api_router.include_router(photos.router, prefix="/photo", tags=["photo"])
 api_router.include_router(questionnaire.router, prefix="/questionnaire", tags=["questionnaire"])
 api_router.include_router(matches.router, prefix="/matches", tags=["matches"])
-api_router.include_router(journey.router, prefix="/journeys", tags=["journey"])
+api_router.include_router(journey.router, prefix="/journey", tags=["journey"])
 api_router.include_router(admin.router, prefix="/admin", tags=["admin"])
